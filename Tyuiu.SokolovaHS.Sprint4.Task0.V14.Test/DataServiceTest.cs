5f7e8585942b8cd684b80333c10eba4610a39639
SokolovaChristina <Tyuiu.SokolovaHS@outlook.com>
2025-11-03 02:33:17 +0500

    Add project files.

---
+++ Tyuiu.SokolovaHS.Sprint4.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.SokolovaHS.Sprint4.Task0.V14.Lib;
+
+namespace Tyuiu.SokolovaHS.Sprint4.Task0.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 8, 9, 5, 4, 3, 2, 1, 0, 5, 7 };
+            int wait = 30; // 9 + 5 + 3 + 1 + 5 + 7 = 30
+
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void GetSumOddArrEl_WithAllEvenNumbers()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 2, 4, 6, 8, 10 };
+            int wait = 0;
+
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void GetSumOddArrEl_WithAllOddNumbers()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 1, 3, 5, 7, 9 };
+            int wait = 25; // 1 + 3 + 5 + 7 + 9 = 25
+
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void GetSumOddArrEl_WithEmptyArray()
+        {
+            DataService ds = new DataService();
+
+            int[] array = new int[0];
+            int wait = 0;
+
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void GetSumOddArrEl_WithSingleOddElement()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 7 };
+            int wait = 7;
+
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void GetSumOddArrEl_WithSingleEvenElement()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 4 };
+            int wait = 0;
+
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void GetSumOddArrEl_WithNegativeOddNumbers()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { -1, -3, 2, 4, -5 };
+            int wait = -9; // -1 + (-3) + (-5) = -9
+
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SokolovaHS.Sprint4.Task0.V14/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaHS.Sprint4.Task0.V14.Lib;
+
+namespace Tyuiu.SokolovaHS.Sprint2.Task1.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнила: Соколова Х. С. | ПКТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы(статический ввод)                              *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнила: Соколова Христина Сергеевна | ПКТБ-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Подсчитать сумму нечетных элементов массива                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            // Исходный массив по заданию
+            int[] array = new int[] { 8, 9, 5, 4, 3, 2, 1, 0, 5, 7 };
+
+            // Вывод массива
+            Console.WriteLine("Массив: { " + string.Join(", ", array) + " }");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            // Создание объекта DataService и вызов метода
+            DataService ds = new DataService();
+            int result = ds.GetSumOddArrEl(array);
+
+            Console.WriteLine("Сумма нечетных элементов массива: " + result);
+
+            // Дополнительная информация для наглядности
+            Console.WriteLine();
+            Console.WriteLine("Нечетные элементы: " + GetOddElementsString(array));
+            Console.WriteLine("Вычисление: " + GetCalculationString(array) + " = " + result);
+
+            Console.ReadLine();
+        }
+
+        // Вспомогательный метод для вывода нечетных элементов
+        static string GetOddElementsString(int[] array)
+        {
+            List<int> oddElements = new List<int>();
+            foreach (int num in array)
+            {
+                if (num % 2 != 0)
+                {
+                    oddElements.Add(num);
+                }
+            }
+            return "{ " + string.Join(", ", oddElements) + " }";
+        }
+
+        // Вспомогательный метод для вывода вычислений
+        static string GetCalculationString(int[] array)
+        {
+            List<string> calculations = new List<string>();
+            foreach (int num in array)
+            {
+                if (num % 2 != 0)
+                {
+                    calculations.Add(num.ToString());
+                }
+            }
+            return string.Join(" + ", calculations);
+        }
+    }
+}
